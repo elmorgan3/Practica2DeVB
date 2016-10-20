3d270f177c395c40bfbdf446c277fd1fa6f93d5d
Morgan <morgan.cordoba@hotmail.com>
2016-10-20 16:18:41 +0200

    Practica 2 de Visual Basic

---
+++ Prac2 ordenar numeros/Form2.Designer.vb
@@ -0,0 +1,403 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.ButtonOrd = New System.Windows.Forms.Button()
+        Me.RadioButton_Asc = New System.Windows.Forms.RadioButton()
+        Me.RadioButton_Des = New System.Windows.Forms.RadioButton()
+        Me.ButtonDivisibles = New System.Windows.Forms.Button()
+        Me.TextBoxDivisor = New System.Windows.Forms.TextBox()
+        Me.Divisor = New System.Windows.Forms.Label()
+        Me.TextBoxMes = New System.Windows.Forms.TextBox()
+        Me.TextBoxAño = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.ButtonDia = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(80, 30)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Tag = ""
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(80, 56)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Tag = ""
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(80, 82)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 2
+        Me.TextBox3.Tag = ""
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(80, 109)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 3
+        Me.TextBox4.Tag = ""
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(80, 136)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 4
+        Me.TextBox5.Tag = ""
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(80, 163)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox6.TabIndex = 5
+        Me.TextBox6.Tag = ""
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(80, 190)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox7.TabIndex = 6
+        Me.TextBox7.Tag = ""
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(80, 217)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox8.TabIndex = 7
+        Me.TextBox8.Tag = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Numero 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Numero 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Numero 3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(21, 112)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Numero 4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(21, 139)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Numero 5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(21, 166)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(53, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Numero 6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(21, 193)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(53, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Numero 7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(21, 220)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(53, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Numero 8"
+        '
+        'ButtonOrd
+        '
+        Me.ButtonOrd.Location = New System.Drawing.Point(24, 347)
+        Me.ButtonOrd.Name = "ButtonOrd"
+        Me.ButtonOrd.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonOrd.TabIndex = 10
+        Me.ButtonOrd.Text = "Ordenar"
+        Me.ButtonOrd.UseVisualStyleBackColor = True
+        '
+        'RadioButton_Asc
+        '
+        Me.RadioButton_Asc.AutoSize = True
+        Me.RadioButton_Asc.Checked = True
+        Me.RadioButton_Asc.Location = New System.Drawing.Point(24, 284)
+        Me.RadioButton_Asc.Name = "RadioButton_Asc"
+        Me.RadioButton_Asc.Size = New System.Drawing.Size(82, 17)
+        Me.RadioButton_Asc.TabIndex = 8
+        Me.RadioButton_Asc.TabStop = True
+        Me.RadioButton_Asc.Text = "Ascendente"
+        Me.RadioButton_Asc.UseVisualStyleBackColor = True
+        '
+        'RadioButton_Des
+        '
+        Me.RadioButton_Des.AutoSize = True
+        Me.RadioButton_Des.Location = New System.Drawing.Point(24, 307)
+        Me.RadioButton_Des.Name = "RadioButton_Des"
+        Me.RadioButton_Des.Size = New System.Drawing.Size(89, 17)
+        Me.RadioButton_Des.TabIndex = 9
+        Me.RadioButton_Des.TabStop = True
+        Me.RadioButton_Des.Text = "Descendente"
+        Me.RadioButton_Des.UseVisualStyleBackColor = True
+        '
+        'ButtonDivisibles
+        '
+        Me.ButtonDivisibles.Location = New System.Drawing.Point(67, 67)
+        Me.ButtonDivisibles.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.ButtonDivisibles.Name = "ButtonDivisibles"
+        Me.ButtonDivisibles.Size = New System.Drawing.Size(56, 19)
+        Me.ButtonDivisibles.TabIndex = 12
+        Me.ButtonDivisibles.Text = "Divisibles"
+        Me.ButtonDivisibles.UseVisualStyleBackColor = True
+        '
+        'TextBoxDivisor
+        '
+        Me.TextBoxDivisor.Location = New System.Drawing.Point(67, 40)
+        Me.TextBoxDivisor.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBoxDivisor.Name = "TextBoxDivisor"
+        Me.TextBoxDivisor.Size = New System.Drawing.Size(76, 20)
+        Me.TextBoxDivisor.TabIndex = 11
+        '
+        'Divisor
+        '
+        Me.Divisor.AutoSize = True
+        Me.Divisor.Location = New System.Drawing.Point(14, 40)
+        Me.Divisor.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Divisor.Name = "Divisor"
+        Me.Divisor.Size = New System.Drawing.Size(39, 13)
+        Me.Divisor.TabIndex = 21
+        Me.Divisor.Text = "Divisor"
+        '
+        'TextBoxMes
+        '
+        Me.TextBoxMes.Location = New System.Drawing.Point(67, 31)
+        Me.TextBoxMes.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBoxMes.Name = "TextBoxMes"
+        Me.TextBoxMes.Size = New System.Drawing.Size(76, 20)
+        Me.TextBoxMes.TabIndex = 13
+        '
+        'TextBoxAño
+        '
+        Me.TextBoxAño.Location = New System.Drawing.Point(67, 68)
+        Me.TextBoxAño.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBoxAño.Name = "TextBoxAño"
+        Me.TextBoxAño.Size = New System.Drawing.Size(76, 20)
+        Me.TextBoxAño.TabIndex = 14
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(14, 33)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(27, 13)
+        Me.Label9.TabIndex = 24
+        Me.Label9.Text = "Mes"
+        '
+        'ButtonDia
+        '
+        Me.ButtonDia.Location = New System.Drawing.Point(67, 119)
+        Me.ButtonDia.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.ButtonDia.Name = "ButtonDia"
+        Me.ButtonDia.Size = New System.Drawing.Size(56, 19)
+        Me.ButtonDia.TabIndex = 15
+        Me.ButtonDia.Text = "Dia"
+        Me.ButtonDia.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(191, 373)
+        Me.GroupBox1.TabIndex = 26
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ordenacion de numeros"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ButtonDivisibles)
+        Me.GroupBox2.Controls.Add(Me.TextBoxDivisor)
+        Me.GroupBox2.Controls.Add(Me.Divisor)
+        Me.GroupBox2.Location = New System.Drawing.Point(244, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox2.TabIndex = 27
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Numero divisible"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label10)
+        Me.GroupBox3.Controls.Add(Me.Label9)
+        Me.GroupBox3.Controls.Add(Me.ButtonDia)
+        Me.GroupBox3.Controls.Add(Me.TextBoxAño)
+        Me.GroupBox3.Controls.Add(Me.TextBoxMes)
+        Me.GroupBox3.Location = New System.Drawing.Point(244, 166)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(262, 158)
+        Me.GroupBox3.TabIndex = 28
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Fechas"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(18, 71)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(26, 13)
+        Me.Label10.TabIndex = 26
+        Me.Label10.Text = "Año"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(594, 483)
+        Me.Controls.Add(Me.RadioButton_Des)
+        Me.Controls.Add(Me.RadioButton_Asc)
+        Me.Controls.Add(Me.ButtonOrd)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents ButtonOrd As System.Windows.Forms.Button
+    Friend WithEvents RadioButton_Asc As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton_Des As System.Windows.Forms.RadioButton
+    Friend WithEvents ButtonDivisibles As Button
+    Friend WithEvents TextBoxDivisor As TextBox
+    Friend WithEvents Divisor As Label
+    Friend WithEvents TextBoxMes As TextBox
+    Friend WithEvents TextBoxAño As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents ButtonDia As Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label10 As Label
+End Class
---
+++ Prac2 ordenar numeros/Form2.vb
@@ -0,0 +1,191 @@
+﻿Public Class Form2
+    Private Sub BtnOrd_Click(sender As Object, e As EventArgs) Handles ButtonOrd.Click
+        Dim numeros(7) As Integer, I, J, aux As Integer
+        'cremaos la array y metemos los numeros que han introducido
+
+        Try
+            numeros(0) = TextBox1.Text
+            TextBox1.BackColor = Color.White
+
+        Catch
+            TextBox1.Focus()
+            TextBox1.BackColor = Color.Red
+            MsgBox("Hay un error en el numero 1")
+            Exit Sub
+        End Try
+
+
+        Try
+            numeros(1) = TextBox2.Text
+            TextBox2.BackColor = Color.White
+
+        Catch
+            TextBox2.Focus()
+            TextBox2.BackColor = Color.Red
+            MsgBox("Hay un error en el numero 2")
+            Exit Sub
+        End Try
+
+
+        Try
+            numeros(2) = TextBox3.Text
+            TextBox3.BackColor = Color.White
+
+        Catch
+            TextBox3.Focus()
+            TextBox3.BackColor = Color.Red
+            MsgBox("Hay un error en el numero 3")
+            Exit Sub
+        End Try
+
+        Try
+            numeros(3) = TextBox4.Text
+            TextBox4.BackColor = Color.White
+
+        Catch
+            TextBox4.Focus()
+            TextBox4.BackColor = Color.Red
+            MsgBox("Hay un error en el numero 4")
+            Exit Sub
+        End Try
+
+
+        Try
+
+            numeros(4) = TextBox5.Text
+            TextBox5.BackColor = Color.White
+
+        Catch
+            TextBox5.Focus()
+            TextBox5.BackColor = Color.Red
+            MsgBox("Hay un error en el numero 5")
+            Exit Sub
+        End Try
+
+
+        Try
+
+            numeros(5) = TextBox6.Text
+            TextBox6.BackColor = Color.White
+
+        Catch
+            TextBox6.Focus()
+            TextBox6.BackColor = Color.Red
+            MsgBox("Hay un error en el numero 6")
+            Exit Sub
+        End Try
+
+
+        Try
+
+            numeros(6) = TextBox7.Text
+            TextBox7.BackColor = Color.White
+
+        Catch
+            TextBox7.Focus()
+            TextBox7.BackColor = Color.Red
+            MsgBox("Hay un error en el numero 7")
+            Exit Sub
+        End Try
+
+
+        Try
+
+            numeros(7) = TextBox8.Text
+            TextBox8.BackColor = Color.White
+
+        Catch
+            TextBox8.Focus()
+            TextBox8.BackColor = Color.Red
+            MsgBox("Hay un error en el numero 8")
+
+            Exit Sub
+        End Try
+
+
+
+
+
+        'Hacemos un bucle dentro para otro para ordenar todos los numeros
+        If (RadioButton_Asc.Checked = True) Then
+            For I = 0 To 7
+                For J = 0 To 7
+                    If numeros(I) < numeros(J) Then
+                        aux = numeros(J)
+                        numeros(J) = numeros(I)
+                        numeros(I) = aux
+                    End If
+                Next J
+            Next I
+
+
+
+        ElseIf (RadioButton_Asc.Checked = False) Then
+            For I = 0 To 7
+                For J = 0 To 7
+                    If numeros(I) > numeros(J) Then
+                        aux = numeros(J)
+                        numeros(J) = numeros(I)
+                        numeros(I) = aux
+                    End If
+                Next J
+            Next I
+
+
+        End If
+
+        TextBox1.Text = numeros(0)
+        TextBox2.Text = numeros(1)
+        TextBox3.Text = numeros(2)
+        TextBox4.Text = numeros(3)
+        TextBox5.Text = numeros(4)
+        TextBox6.Text = numeros(5)
+        TextBox7.Text = numeros(6)
+        TextBox8.Text = numeros(7)
+    End Sub
+
+    Private Sub ButtonDivisibles_Click(sender As Object, e As EventArgs) Handles ButtonDivisibles.Click
+        Dim numDivisible, operacion, i As Double
+        Dim numeros(7) As Integer
+        Dim contador As Integer = 0
+        'cremaos la array y metemos los numeros que han introducido
+        numeros(0) = TextBox1.Text
+        numeros(1) = TextBox2.Text
+        numeros(2) = TextBox3.Text
+        numeros(3) = TextBox4.Text
+        numeros(4) = TextBox5.Text
+        numeros(5) = TextBox6.Text
+        numeros(6) = TextBox7.Text
+        numeros(7) = TextBox8.Text
+
+        'Ahora comprobamos que el numero que han introduido no en un 0
+        If (TextBoxDivisor.Text = "0" Or TextBoxDivisor.Text = "") Then
+            MsgBox("No se puede divir entre cero")
+        Else
+            If (TextBoxDivisor.Text <> "0") Then
+                For i = 0 To 7
+                    operacion = numeros(i) / TextBoxDivisor.Text
+                    If numeros(i) Mod TextBoxDivisor.Text = 0 Then
+                        contador = contador + 1
+                    End If
+                Next i
+            End If
+        End If
+        'Mostramos en un msgBox cuantos ha habido
+        MsgBox(contador)
+
+    End Sub
+
+    Private Sub ButtonDia_Click(sender As Object, e As EventArgs) Handles ButtonDia.Click
+        If TextBoxMes.Text = "1" Or TextBoxMes.Text = "3" Or TextBoxMes.Text = "5" Or TextBoxMes.Text = "7" Or TextBoxMes.Text = "8" Or TextBoxMes.Text = "10" Or TextBoxMes.Text = "12" Then
+            MsgBox("31")
+
+        ElseIf TextBoxMes.Text = "4" Or TextBoxMes.Text = "6" Or TextBoxMes.Text = "9" Or TextBoxMes.Text = "11" Then
+            MsgBox("30")
+        ElseIf TextBoxMes.Text = "2" Then
+            MsgBox("28")
+        Else
+            MsgBox("No existe el mes indicado")
+        End If
+    End Sub
+End Class
